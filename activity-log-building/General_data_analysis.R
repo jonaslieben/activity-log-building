bc4e476cd86fe54a06cc3fb5d3ff76439d14c0fd
Jonas Lieben <jonas.lieben@agilians.com>
2016-11-02 11:35:30 +0100

    added general data analysis file and thought of some interesting insights

---
+++ activity-log-building/General_data_analysis.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(lubridate)
+
+#Amount of commits
+
+#Amount of unique files currently in project
+
+#Amount of unique files which have been in the project
+
+#Amount of people who contributed
+
+#Commits over time
+
+#file changes/added/deleted over time
