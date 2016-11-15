550b0342560315035ee848c149d9eb750429b209
Jonas Lieben <jonas.lieben@me.com>
2016-11-15 21:31:09 +0100

    added function for generating beginning timestamps (preliminary still not working)

--- activity-log-building/Calculate_right_beginning_timestamp.R
+++ activity-log-building/Calculate_right_beginning_timestamp.R
@@ -9,7 +9,7 @@ removeBeginningTimeStampFromOutlierObservations <- function(eventDataTable) {
   
   #find all the authors which contributed to the project
   authors <- eventDataTable %>% select(author) %>% distinct()
-  sum <- 0
+  
   for(i in 1:length(authors$author)) {
     #filter the eventdatatable for the rows of only author with index i and where the duration is a number
     eventDataTableForAuthor <- eventDataTableTemp %>% filter(author == as.character(authors$author[i])) %>% filter(!is.na(duration))
@@ -19,7 +19,7 @@ removeBeginningTimeStampFromOutlierObservations <- function(eventDataTable) {
     
     #find all the identifiers which have a duration bigger than two times the standard deviation plus the average
     outlierIdentifiers <- eventDataTableForAuthor %>% select(identifier, duration) %>% filter(duration >  2 * SDDuration + averageDuration)
-    sum <- sum + length(outlierIdentifiers)
+    
     for(i in 1:length(eventDataTableTemp$identifier)) {
       #assign NA to the beginning timestamp if the identifier of a row matches one of the outlierIdentifiers
       if(eventDataTable$identifier[i] %in% outlierIdentifiers) {
@@ -31,3 +31,24 @@ removeBeginningTimeStampFromOutlierObservations <- function(eventDataTable) {
   return(eventDataTable)
 }
 
+addBeginningTimeStampsToFirstCommits <- function(eventDataTable) {
+  
+  #create three new variables: one for the begin timestamp in a lubridate object, one for the end timestamp in a lubridate object and one for the duration between the two
+  eventDataTableTemp <- eventDataTable %>%
+    mutate(start = ymd_hms(beginningTimestamp)) %>% 
+    mutate(end = ymd_hms(endTimestamp)) %>% 
+    mutate(duration = as.numeric(difftime(start, end, units = "secs")))
+  
+  #find all the authors which contributed to the project
+  authors <- eventDataTable %>% select(author) %>% distinct()
+  
+  for(i in 1:length(authors$author)) {
+    eventDataTableForAuthor <- eventDataTableTemp %>% filter(author == as.character(authors$author[i])) %>% filter(!is.na(duration))
+    averageDuration <- eventDataTableForAuthor %>% summarise(avg = mean(duration))
+    for(i in 1:length(eventDataTableTemp$identifier)) {
+      if(is.na(eventDataTable$beginningTimestamp[i])) {
+        
+      }
+    }
+  }
+}
